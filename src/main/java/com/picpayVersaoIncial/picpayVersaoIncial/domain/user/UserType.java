50b300f6c0956689c75bf56718c807c59213f5b1
GabrielMM182 <gmm_morais@hotmail.com>
2024-12-18 15:56:50 -0300

    fix: fix lombok bug dont generate get if expected, now the app is running

--- src/main/java/com/picpayVersaoIncial/picpayVersaoIncial/domain/user/UserType.java
+++ src/main/java/com/picpayVersaoIncial/picpayVersaoIncial/domain/user/UserType.java
@@ -1,16 +1,6 @@
 package com.picpayVersaoIncial.picpayVersaoIncial.domain.user;
 
 public enum UserType {
-    COMMON("Usuário Comum"),
-    MERCHANT("Comerciante");
-
-    private final String description;
-
-    UserType(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+    COMMON,
+    MERCHANT
 }
