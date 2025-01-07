a7885dbf0426d0e8ed9d023e5976cad9d9d3a048
GabrielMM182 <gmm_morais@hotmail.com>
2025-01-07 00:27:43 -0300

    FEAT: Create TransactionServiceTest

--- src/test/java/com/picpayVersaoIncial/picpayVersaoIncial/services/TransactionServiceTest.java
+++ src/test/java/com/picpayVersaoIncial/picpayVersaoIncial/services/TransactionServiceTest.java
@@ -1,12 +1,90 @@
 package com.picpayVersaoIncial.picpayVersaoIncial.services;
 
+import com.picpayVersaoIncial.picpayVersaoIncial.domain.user.User;
+import com.picpayVersaoIncial.picpayVersaoIncial.domain.user.UserType;
+import com.picpayVersaoIncial.picpayVersaoIncial.dtos.TransactionDTO;
+import com.picpayVersaoIncial.picpayVersaoIncial.repositories.TransactionRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class TransactionServiceTest {
 
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private TransactionRepository repository;
+
+    @Mock
+    private AuthorizationService authService;
+
+    @Mock
+    private NotificationService notificationService;
+
+    @Autowired
+    @InjectMocks
+    private TransactionService transactionService;
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+
     @Test
-    void createTransaction() {
+    @DisplayName("Should create transaction successfully when everything is ok")
+    void createTransactionCase1() throws Exception {
+
+        User sender = new User(1L, "Maria", "Souza", "99999999901", "maria@gmail.com", "12345", new BigDecimal(10), UserType.COMMON);
+        User receiver = new User(2L, "Joao", "Souza", "99999999902", "joao@gmail.com", "12345", new BigDecimal(10), UserType.COMMON);
+
+        when(userService.findUserById(1L)).thenReturn(sender);
+        when(userService.findUserById(2L)).thenReturn(receiver);
+
+        when(authService.authorizeTransaction(any(), any())).thenReturn(true);
+
+        TransactionDTO request = new TransactionDTO(new BigDecimal(10), 1L, 2L);
+        transactionService.createTransaction(request);
+
+        verify(repository, times(1)).save(any());
+
+        sender.setBalance(new BigDecimal(0));
+        verify(userService, times(1)).saveUser(sender);
+
+        receiver.setBalance(new BigDecimal(20));
+        verify(userService, times(1)).saveUser(receiver);
+
+        verify(notificationService, times(1)).sendNotification(sender, "Transaction success");
+        verify(notificationService, times(1)).sendNotification(receiver, "Transaction success");
+    }
+
+    @Test
+    @DisplayName("Should throw Exception when Transaction is not allowed")
+    void createTransactionCase2() throws Exception {
+        User sender = new User(1L, "Maria", "Souza", "99999999901", "maria@gmail.com", "12345", new BigDecimal(10), UserType.MERCHANT);
+        User receiver = new User(2L, "Joao", "Souza", "99999999902", "joao@gmail.com", "12345", new BigDecimal(10), UserType.COMMON);
+
+        when(userService.findUserById(1L)).thenReturn(sender);
+        when(userService.findUserById(2L)).thenReturn(receiver);
+
+        when(authService.authorizeTransaction(any(), any())).thenReturn(false);
+
+        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+            TransactionDTO request = new TransactionDTO(new BigDecimal(10), 1L, 2L);
+            transactionService.createTransaction(request);
+        });
+
+        Assertions.assertEquals("Transação não autorizada", thrown.getMessage());
     }
 }
